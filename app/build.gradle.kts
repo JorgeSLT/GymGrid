e325dd75ba1b27400b1e9e7650e55ffab406e25f
JorgeSLT <jorgesltm@gmail.com>
2024-03-24 21:38:29 +0100

    Rediseño
    Rediseño y configuración básica

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
     }
     buildFeatures {
         compose = true
+        viewBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
@@ -58,6 +59,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -65,4 +67,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
 }
--- app/src/main/java/com/example/gymgrid/MainActivity.kt
+++ app/src/main/java/com/example/gymgrid/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.gymgrid
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import com.example.gymgrid.databinding.BarsBinding
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var binding: BarsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = BarsBinding.inflate(layoutInflater)
+
+
+        binding.backButton.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
