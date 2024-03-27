4ab5ed7618183e9e5d278f33083f346970133938
JorgeSLT <jorgesltm@gmail.com>
2024-03-27 17:22:14 +0100

    Frament config v2 (Still not working)

--- app/src/main/java/com/example/gymgrid/MainActivity.kt
+++ app/src/main/java/com/example/gymgrid/MainActivity.kt
@@ -1,27 +1,46 @@
 package com.example.gymgrid
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.gymgrid.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        binding.startButton.setOnClickListener {
-            val navController = findNavController(R.id.nav_host_fragment_content_main)
-            navController.navigate(R.id.trainingFragment)
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        val currentDestinationId = navController.currentDestination?.id
+
+        // Que fragmento
+        when (currentDestinationId) {
+            R.id.homeFragment -> {
+                navController.navigate(R.id.action_homeFragment_to_trainingFragment)
+            }
+
+            R.id.trainingFragment -> {
+                navController.navigate(R.id.action_trainingFragment_to_homeFragment)
+            }
+
+            else -> {
+
+            }
         }
-        binding.backButton.setOnClickListener {
-            onBackPressed()
-        }
+
     }
 }
---
+++ app/src/main/java/com/example/gymgrid/ui/theme/HomeFragment.kt
@@ -0,0 +1,34 @@
+package com.example.gymgrid
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.ComponentActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.gymgrid.databinding.HomeFragmentBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: HomeFragmentBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = HomeFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
