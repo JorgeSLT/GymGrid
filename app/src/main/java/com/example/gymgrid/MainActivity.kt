2f6f14895d273b38f7bb453ae9546733b53693f8
JorgeSLT <jorgesltm@gmail.com>
2024-03-29 13:10:33 +0100

    Navigation updated
    Navigation is almost done, just have to repeat the process for each page.

--- app/src/main/java/com/example/gymgrid/MainActivity.kt
+++ app/src/main/java/com/example/gymgrid/MainActivity.kt
@@ -4,15 +4,15 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.gymgrid.databinding.ActivityMainBinding
 
-
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,21 +21,37 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val toolbar: Toolbar = binding.toolbar // Asumiendo que tienes un Toolbar en tu layout con el id 'toolbar'
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false) // Oculta el título predeterminado de la ActionBar
+
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
 
-        val currentDestinationId = navController.currentDestination?.id
-
+        // Configuración de la acción del botón de inicio
         binding.startButton.setOnClickListener { _ ->
-            if(currentDestinationId == R.id.homeFragment){
-                navController.navigate(R.id.action_homeFragment_to_trainingFragment)
+            val currentDestinationId = navController.currentDestination?.id
+            when (currentDestinationId) {
+                R.id.homeFragment -> navController.navigate(R.id.action_homeFragment_to_trainingFragment)
+                R.id.trainingFragment -> navController.navigate(R.id.action_trainingFragment_to_homeFragment)
             }
-            else if(currentDestinationId == R.id.trainingFragment){
-                navController.navigate(R.id.action_trainingFragment_to_homeFragment)
-            }
-
         }
-
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_perfil -> {
+                val navController = findNavController(R.id.nav_host_fragment_content_main)
+
+                navController.navigate(R.id.action_global_perfilFragment)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
