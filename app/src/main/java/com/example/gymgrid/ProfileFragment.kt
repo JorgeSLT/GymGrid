96dfac122b1d6830d6cfdb368f49af63ebe31c56
JorgeSLT <jorgesltm@gmail.com>
2024-03-30 20:59:37 +0100

    Basic profile done
    Can add custom image and routine strike.

--- app/src/main/java/com/example/gymgrid/ProfileFragment.kt
+++ app/src/main/java/com/example/gymgrid/ProfileFragment.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.example.gymgrid.databinding.ProfileFragmentBinding
 
@@ -28,6 +30,17 @@ class ProfileFragment : Fragment() {
         showUserName()
         showUserGoal()
         showUserDays()
+
+        binding.editProfileButton.setOnClickListener {
+            editUserName()
+        }
+
+        binding.editGoalButton.setOnClickListener {
+            editUserGoal()
+        }
+        binding.editDaysButton.setOnClickListener {
+            editUserDays()
+        }
     }
 
     private fun showUserName() {
@@ -47,6 +60,86 @@ class ProfileFragment : Fragment() {
         val userName = sharedPref?.getString("TRAINING_DAYS", "3 días") ?: "3 días"
         binding.profileDaysText.text = userName
     }
+
+    private fun editUserName() {
+        val layoutInflater = LayoutInflater.from(context)
+        val view = layoutInflater.inflate(R.layout.dialog_custom_layout, null)
+        val editText = view.findViewById<EditText>(R.id.editTextUserName)
+        editText.setText(binding.profileNameText.text)
+
+        AlertDialog.Builder(requireContext())
+            .setTitle("Editar nombre")
+            .setView(view)
+            .setPositiveButton("OK") { _, _ ->
+                val newName = editText.text.toString().trim()
+                if (newName.isNotEmpty()) {
+                    saveUserName(newName)
+                    binding.profileNameText.text = newName
+                }
+            }
+            .create().show()
+    }
+
+    private fun saveUserName(name: String) {
+        val sharedPref = activity?.getSharedPreferences("userPreferences", Context.MODE_PRIVATE) ?: return
+        with(sharedPref.edit()) {
+            putString("USER_NAME", name)
+            putBoolean("FIRST_TIME", false)
+            apply()
+        }
+        binding.profileNameText.text = name
+    }
+
+    private fun editUserGoal() {
+        val goals = arrayOf("Perder peso", "Ganar músculo")
+        val currentGoal = binding.profileGoalText.text.toString()
+
+        val selectedGoalIndex = goals.indexOfFirst { it == currentGoal }.takeIf { it >= 0 } ?: 0
+
+        AlertDialog.Builder(requireContext())
+            .setTitle("Editar objetivo")
+            .setSingleChoiceItems(goals, selectedGoalIndex) { dialog, which ->
+                val selectedGoal = goals[which]
+                saveUserGoal(selectedGoal)
+                binding.profileGoalText.text = selectedGoal
+                dialog.dismiss()
+            }
+            .create().show()
+    }
+
+    private fun saveUserGoal(goal: String) {
+        val sharedPref = activity?.getSharedPreferences("userPreferences", Context.MODE_PRIVATE) ?: return
+        with(sharedPref.edit()) {
+            putString("FITNESS_GOAL", goal)
+            apply()
+        }
+    }
+
+    private fun editUserDays() {
+        val daysOptions = arrayOf("3 días", "5 días")
+        val currentDays = binding.profileDaysText.text.toString()
+
+        val selectedDaysIndex = daysOptions.indexOfFirst { it == currentDays }.takeIf { it >= 0 } ?: 0
+
+        AlertDialog.Builder(requireContext())
+            .setTitle("Editar días de entrenamiento")
+            .setSingleChoiceItems(daysOptions, selectedDaysIndex) { dialog, which ->
+                val selectedDays = daysOptions[which]
+                saveUserDays(selectedDays)
+                binding.profileDaysText.text = selectedDays
+                dialog.dismiss()
+            }
+            .create().show()
+    }
+
+    private fun saveUserDays(days: String) {
+        val sharedPref = activity?.getSharedPreferences("userPreferences", Context.MODE_PRIVATE) ?: return
+        with(sharedPref.edit()) {
+            putString("TRAINING_DAYS", days)
+            apply()
+        }
+    }
+
 
 
     override fun onDestroyView() {
