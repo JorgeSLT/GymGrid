b656df22ec753d157ee6422b7e007e082efb2c6e
JorgeSLT <jorgesltm@gmail.com>
2024-03-30 19:17:04 +0100

    Profile updated
    Need to add edit function

--- app/src/main/java/com/example/gymgrid/MainActivity.kt
+++ app/src/main/java/com/example/gymgrid/MainActivity.kt
@@ -1,84 +1,171 @@
-package com.example.gymgrid
+    package com.example.gymgrid
 
-import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.navigation.findNavController
-import com.example.gymgrid.databinding.ActivityMainBinding
+    import android.content.Context
+    import android.os.Bundle
+    import android.view.LayoutInflater
+    import android.view.Menu
+    import android.view.MenuItem
+    import androidx.appcompat.app.AppCompatActivity
+    import androidx.appcompat.widget.Toolbar
+    import androidx.navigation.findNavController
+    import com.example.gymgrid.databinding.ActivityMainBinding
+    import android.widget.EditText
+    import androidx.appcompat.app.AlertDialog
 
-class MainActivity : AppCompatActivity() {
+    class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+        private lateinit var binding: ActivityMainBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
 
-        val toolbar: Toolbar = binding.toolbar
-        setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayShowTitleEnabled(false)
+            val toolbar: Toolbar = binding.toolbar
+            setSupportActionBar(toolbar)
+            supportActionBar?.setDisplayShowTitleEnabled(false)
 
-        setupButtonActions()
-        setupToolbarTitle()
-    }
-
-    private fun setupButtonActions() {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-
-        binding.homeButton.setOnClickListener {
-            navigateToFragment(R.id.action_global_homeFragment)
-        }
-
-        binding.startButton.setOnClickListener {
-            navigateToFragment(R.id.action_global_trainingFragment)
-        }
-
-        binding.calendarButton.setOnClickListener {
-            navigateToFragment(R.id.action_global_calendarFragment)
-        }
-
-        binding.routineButton.setOnClickListener {
-            navigateToFragment(R.id.action_global_routineFragment)
-        }
-    }
-
-    private fun setupToolbarTitle() {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            val title = when (destination.id) {
-                R.id.homeFragment -> "Inicio"
-                R.id.trainingFragment -> "Entrenamiento"
-                R.id.calendarFragment -> "Calendario"
-                R.id.routineFragment -> "Rutina"
-                R.id.profileFragment -> "Perfil"
-
-                else -> "Título de Página"
+            if (isFirstTime()) {
+                askForUserName()
             }
-            binding.toolbarTitle.text = title
+
+            setupButtonActions()
+            setupToolbarTitle()
         }
-    }
 
-    private fun navigateToFragment(actionId: Int) {
-        findNavController(R.id.nav_host_fragment_content_main).navigate(actionId)
-    }
+        private fun setupButtonActions() {
+            val navController = findNavController(R.id.nav_host_fragment_content_main)
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.action_perfil -> {
-                navigateToFragment(R.id.action_global_profileFragment)
-                true
+            binding.homeButton.setOnClickListener {
+                navigateToFragment(R.id.action_global_homeFragment)
             }
-            else -> super.onOptionsItemSelected(item)
+
+            binding.startButton.setOnClickListener {
+                navigateToFragment(R.id.action_global_trainingFragment)
+            }
+
+            binding.calendarButton.setOnClickListener {
+                navigateToFragment(R.id.action_global_calendarFragment)
+            }
+
+            binding.routineButton.setOnClickListener {
+                navigateToFragment(R.id.action_global_routineFragment)
+            }
+        }
+
+        private fun setupToolbarTitle() {
+            val navController = findNavController(R.id.nav_host_fragment_content_main)
+
+            navController.addOnDestinationChangedListener { _, destination, _ ->
+                val title = when (destination.id) {
+                    R.id.homeFragment -> "Inicio"
+                    R.id.trainingFragment -> "Entrenamiento"
+                    R.id.calendarFragment -> "Calendario"
+                    R.id.routineFragment -> "Rutina"
+                    R.id.profileFragment -> "Perfil"
+
+                    else -> "Título de Página"
+                }
+                binding.toolbarTitle.text = title
+            }
+        }
+
+        private fun navigateToFragment(actionId: Int) {
+            findNavController(R.id.nav_host_fragment_content_main).navigate(actionId)
+        }
+
+        override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+            menuInflater.inflate(R.menu.menu_main, menu)
+            return true
+        }
+
+        override fun onOptionsItemSelected(item: MenuItem): Boolean {
+            return when (item.itemId) {
+                R.id.action_perfil -> {
+                    navigateToFragment(R.id.action_global_profileFragment)
+                    true
+                }
+                else -> super.onOptionsItemSelected(item)
+            }
+        }
+
+        private fun askForUserName() {
+            val layoutInflater = LayoutInflater.from(this)
+            val view = layoutInflater.inflate(R.layout.dialog_custom_layout, null)
+            val editText = view.findViewById<EditText>(R.id.editTextUserName)
+
+            AlertDialog.Builder(this)
+                .setTitle("Bienvenido")
+                .setMessage("¿Cómo te llamas?")
+                .setView(view)
+                .setCancelable(false)
+                .setPositiveButton("OK") { _, _ ->
+                    val name = editText.text.toString().trim()
+                    if (name.isNotEmpty()) {
+                        saveUserName(name)
+                        askForFitnessGoal()
+                    }
+                }
+                .create().show()
+        }
+
+        private fun saveUserName(name: String) {
+            val sharedPref = getSharedPreferences("userPreferences", Context.MODE_PRIVATE) ?: return
+            with(sharedPref.edit()) {
+                putString("USER_NAME", name)
+                putBoolean("FIRST_TIME", false)
+                apply()
+            }
+        }
+
+        private fun askForFitnessGoal() {
+            val goals = arrayOf("Perder peso", "Ganar músculo")
+            AlertDialog.Builder(this)
+                .setTitle("Selecciona tu objetivo")
+                .setSingleChoiceItems(goals, -1) { dialog, which ->
+                    saveFitnessGoal(goals[which])
+                    dialog.dismiss()
+                    askForTrainingDays()
+                }
+                .setCancelable(false)
+                .create()
+                .show()
+        }
+
+
+        private fun saveFitnessGoal(goal: String) {
+            val sharedPref = getSharedPreferences("userPreferences", Context.MODE_PRIVATE) ?: return
+            with(sharedPref.edit()) {
+                putString("FITNESS_GOAL", goal)
+                apply()
+            }
+        }
+
+        private fun askForTrainingDays() {
+            val daysOptions = arrayOf("3 días", "5 días")
+            AlertDialog.Builder(this)
+                .setTitle("¿Cuántos días piensas entrenar a la semana?")
+                .setSingleChoiceItems(daysOptions, -1) { dialog, which ->
+                    saveTrainingDays(daysOptions[which])
+                    dialog.dismiss()
+                }
+                .setCancelable(false)
+                .create()
+                .show()
+        }
+
+
+        private fun saveTrainingDays(days: String) {
+            val sharedPref = getSharedPreferences("userPreferences", Context.MODE_PRIVATE) ?: return
+            with(sharedPref.edit()) {
+                putString("TRAINING_DAYS", days)
+                apply()
+            }
+        }
+
+        private fun isFirstTime(): Boolean {
+            val sharedPref = getSharedPreferences("userPreferences", Context.MODE_PRIVATE)
+            return sharedPref.getBoolean("FIRST_TIME", true)
         }
     }
-}
--- app/src/main/java/com/example/gymgrid/ProfileFragment.kt
+++ app/src/main/java/com/example/gymgrid/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.example.gymgrid
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,19 +12,42 @@ class ProfileFragment : Fragment() {
 
     private var _binding: ProfileFragmentBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         _binding = ProfileFragmentBinding.inflate(inflater, container, false)
         return binding.root
-
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        showUserName()
+        showUserGoal()
+        showUserDays()
+    }
+
+    private fun showUserName() {
+        val sharedPref = activity?.getSharedPreferences("userPreferences", Context.MODE_PRIVATE)
+        val userName = sharedPref?.getString("USER_NAME", "Usuario Anónimo") ?: "Usuario Anónimo"
+        binding.profileNameText.text = userName
+    }
+
+    private fun showUserGoal() {
+        val sharedPref = activity?.getSharedPreferences("userPreferences", Context.MODE_PRIVATE)
+        val userName = sharedPref?.getString("FITNESS_GOAL", "Perder peso") ?: "Perder peso"
+        binding.profileGoalText.text = userName
+    }
+
+    private fun showUserDays() {
+        val sharedPref = activity?.getSharedPreferences("userPreferences", Context.MODE_PRIVATE)
+        val userName = sharedPref?.getString("TRAINING_DAYS", "3 días") ?: "3 días"
+        binding.profileDaysText.text = userName
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
